03b81ed9c0c88ee400bee6607b70c7fe82fe8b42
Duc Long <longspeed001@gmail.com>
2025-01-18 08:17:28 +0700

    oke

---
+++ Bai_Tap/bai3.java
@@ -0,0 +1,31 @@
+
+public class bai3 {
+
+    public static int fibonacci(int a) {
+        if (a <= 0) {
+            return 0;
+        }
+        if (a == 1) {
+            return 1;
+        }
+        return fibonacci(a - 1) + fibonacci(a - 2);
+    }
+
+    public static void main(String[] args) {
+        double m = 200;
+        int i = 0;
+        while (true) {
+            if (fibonacci(i) < m) {
+                i++;
+            } else {
+                break;
+            }
+        } 
+        if(m<fibonacci(i +  1)){
+            System.out.println(i);
+        }
+
+    }
+}
+
+   
